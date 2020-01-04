88d9515e758c6442aa2b47479872b607ea4ec510
Parth2501 <53107006+Parth2501@users.noreply.github.com>
2020-01-04 22:03:34 +0530

    final commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/e8f8fab71e96f60e1d1670cf18b9a47e/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
